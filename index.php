9d69677586dfce980d52c428b8ce42892c8f497a
John Conleth <johnconleth@users.noreply.github.com>
2017-07-28 22:56:24 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+    <title>Pretty JSON</title>
+    <body>
+<?php
+if(isset($_POST['json'])){
+    $json = $_POST['json'];
+    $json_array = json_decode($json, true);
+    echo '<pre>';
+    print_r($json_array);
+    echo '</pre>';
+}
+else {
+    ?>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+            <textarea name="json" cols="40" rows="10"></textarea>
+            <input type="submit" name="submit" value="MAKE PRETTY!"/>
+        </form>
+    <?php
+}
+?>
+    </body>
+</html> 
